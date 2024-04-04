7322301a7f2909ec1b323a163ebdfc220201ec7a
K. Cement <kelvin@wcyber.net>
2024-04-04 14:11:55 +0200

    Initial Commit

---
+++ Assignment1/Assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1App.swift
+//  Assignment1
+//
+//  Created by wCyber Solutions on 2024/04/04.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
